93d663076a2fbe6eaea1e1ea847d1d5800499356
Jamie Nicol <jamie@jamiern.uk>
2023-08-02 16:30:06 +0100

    Allow budget and workload to be configured from the UI
    And display average frame duration

--- app/src/main/java/uk/jamiern/perfhintmandemo/GeckoThread.kt
+++ app/src/main/java/uk/jamiern/perfhintmandemo/GeckoThread.kt
@@ -1,13 +1,14 @@
 package uk.jamiern.perfhintmandemo
 
 import android.content.Context
+import android.os.Handler
 import android.os.HandlerThread
+import android.os.Looper
 import android.os.PerformanceHintManager
 import android.os.Process
 import android.util.Log
 import android.view.Choreographer
 import kotlin.time.Duration
-import kotlin.time.Duration.Companion.milliseconds
 import kotlin.time.ExperimentalTime
 import kotlin.time.TimeSource
 import kotlin.time.measureTime
@@ -16,18 +17,44 @@ import kotlin.time.measureTime
 class GeckoThread(name: String, context: Context) : HandlerThread(name),
     Choreographer.FrameCallback {
 
+    companion object {
+        private const val LOGTAG = "GeckoThread"
+    }
+
+    private var workload: Int = 0
+    private var phmEnabled = false
+    private var budget: Duration = Duration.ZERO
+
     private val perfHintManager = context.getSystemService(PerformanceHintManager::class.java)
-    private lateinit var perfHintSession: PerformanceHintManager.Session
+    private var perfHintSession: PerformanceHintManager.Session? = null
     private var frameId = 0
     private var totalDuration: Duration = Duration.ZERO
 
+    fun updateSettings(workload: Int, phmEnabled: Boolean, budget: Duration) {
+        Log.d(
+            LOGTAG,
+            "updateSettings() workload: $workload, PHM enabled: $phmEnabled, budget: $budget"
+        )
+
+        this.workload = workload
+
+        if (phmEnabled && !this.phmEnabled) {
+            perfHintSession = perfHintManager.createHintSession(
+                intArrayOf(Process.myTid()), budget.inWholeNanoseconds
+            )!!
+        } else if (!phmEnabled && this.phmEnabled) {
+            perfHintSession?.close()
+            perfHintSession = null
+        }
+        this.phmEnabled = phmEnabled
+
+        this.budget = budget
+        perfHintSession?.updateTargetWorkDuration(budget.inWholeNanoseconds)
+    }
+
     @Override
     override fun onLooperPrepared() {
-        val tid = Process.myTid()
-        Log.d(LOGTAG, "GeckoThread tid: $tid")
-        perfHintSession =
-            perfHintManager.createHintSession(intArrayOf(tid), target.inWholeNanoseconds)!!
-
+        Log.d(LOGTAG, "GeckoThread tid: ${Process.myTid()}")
         Choreographer.getInstance().postFrameCallback(this)
     }
 
@@ -37,21 +64,27 @@ class GeckoThread(name: String, context: Context) : HandlerThread(name),
 
         @Suppress("ASSIGNED_BUT_NEVER_ACCESSED_VARIABLE") val primes: List<Int>
         val duration = TimeSource.Monotonic.measureTime {
-            primes = calculatePrimes()
+            primes = calculatePrimes(workload)
         }
 
-        perfHintSession.reportActualWorkDuration(duration.inWholeNanoseconds)
+        perfHintSession?.reportActualWorkDuration(duration.inWholeNanoseconds)
         totalDuration += duration
 
         if (frameId == 10) {
-            Log.d(LOGTAG, "Average duration for $frameId frames: ${totalDuration / frameId}")
+            val averageDuration = totalDuration / frameId
+            Log.d(LOGTAG, "Average duration for $frameId frames: $averageDuration")
             frameId = 0
             totalDuration = Duration.ZERO
+
+            val handler = Handler(Looper.getMainLooper())
+            handler.post {
+                MainActivity.result = averageDuration.toString()
+            }
         }
         Choreographer.getInstance().postFrameCallback(this)
     }
 
-    private fun calculatePrimes(): List<Int> {
+    private fun calculatePrimes(workload: Int): List<Int> {
         val primes = mutableListOf<Int>()
         for (n in 2..workload) {
             if ((2 until n).none { n % it == 0 }) {
@@ -59,11 +92,5 @@ class GeckoThread(name: String, context: Context) : HandlerThread(name),
             }
         }
         return primes
-    }
-
-    companion object {
-        private const val LOGTAG = "GeckoThread"
-        private val target = 5.milliseconds
-        private const val workload = 3000
     }
 }
--- app/src/main/java/uk/jamiern/perfhintmandemo/MainActivity.kt
+++ app/src/main/java/uk/jamiern/perfhintmandemo/MainActivity.kt
@@ -1,21 +1,46 @@
 package uk.jamiern.perfhintmandemo
 
 import android.os.Bundle
+import android.os.Handler
 import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.DropdownMenuItem
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.ExposedDropdownMenuBox
+import androidx.compose.material3.ExposedDropdownMenuDefaults.TrailingIcon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Switch
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import uk.jamiern.perfhintmandemo.ui.theme.PerfHintManDemoTheme
+import kotlin.time.Duration.Companion.milliseconds
 
 class MainActivity : ComponentActivity() {
+    companion object {
+        private const val LOGTAG = "MainActivity"
+        var result by mutableStateOf("")
+    }
 
     private lateinit var geckoThread: GeckoThread
+
+    private var phmEnabled = false
+    private val workloadValues = arrayOf(1500, 3000, 5000)
+    private val workloadLabels = arrayOf("Small", "Medium", "Large")
+    private var selectedWorkload = 0
+    private val budgetValues = arrayOf(5.milliseconds, 10.milliseconds, 15.milliseconds)
+    private val budgetLabels = arrayOf("5ms", "10ms", "15ms")
+    private var selectedBudget = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         Log.d(LOGTAG, "MainActivity.onCreate()")
@@ -24,35 +49,96 @@ class MainActivity : ComponentActivity() {
             PerfHintManDemoTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
+                    modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    MainUi()
                 }
             }
         }
 
         geckoThread = GeckoThread("GeckoThread", this)
         geckoThread.start()
+        updateSettings()
     }
 
-    companion object {
-        private const val LOGTAG = "MainActivity"
+    private fun updateSettings() {
+        val handler = Handler(geckoThread.looper)
+        handler.post {
+            geckoThread.updateSettings(
+                workloadValues[selectedWorkload], phmEnabled, budgetValues[selectedBudget]
+            )
+        }
     }
-}
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+    @OptIn(ExperimentalMaterial3Api::class)
+    @Composable
+    fun MainUi() {
+        var workloadExpanded by remember {
+            mutableStateOf(false)
+        }
+        var enabledChecked by remember {
+            mutableStateOf(phmEnabled)
+        }
+        var budgetExpanded by remember {
+            mutableStateOf(false)
+        }
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    PerfHintManDemoTheme {
-        Greeting("Android")
+        Column {
+            Row {
+                Text(text = "Workload")
+                ExposedDropdownMenuBox(expanded = workloadExpanded,
+                    onExpandedChange = { workloadExpanded = !workloadExpanded }) {
+                    TextField(
+                        value = workloadLabels[selectedWorkload],
+                        onValueChange = {},
+                        readOnly = true,
+                        trailingIcon = { TrailingIcon(expanded = workloadExpanded) },
+                        modifier = Modifier.menuAnchor()
+                    )
+                    ExposedDropdownMenu(expanded = workloadExpanded,
+                        onDismissRequest = { workloadExpanded = false }) {
+                        workloadLabels.forEachIndexed { i, label ->
+                            DropdownMenuItem(text = { Text(label) }, onClick = {
+                                selectedWorkload = i
+                                workloadExpanded = false
+                                updateSettings()
+                            })
+                        }
+                    }
+                }
+            }
+            Row {
+                Text(text = "Enable PerformanceHintManager")
+
+                Switch(checked = enabledChecked, onCheckedChange = {
+                    enabledChecked = it
+                    phmEnabled = enabledChecked
+                    updateSettings()
+                })
+            }
+            Row {
+                Text(text = "Budget")
+                ExposedDropdownMenuBox(expanded = budgetExpanded,
+                    onExpandedChange = { budgetExpanded = !budgetExpanded }) {
+                    TextField(value = budgetLabels[selectedBudget],
+                        onValueChange = {},
+                        readOnly = true,
+                        trailingIcon = { TrailingIcon(expanded = budgetExpanded) },
+                        modifier = Modifier.menuAnchor()
+                    )
+                    ExposedDropdownMenu(expanded = budgetExpanded,
+                        onDismissRequest = { budgetExpanded = false }) {
+                        budgetLabels.forEachIndexed { i, label ->
+                            DropdownMenuItem(text = { Text(label) }, onClick = {
+                                selectedBudget = i
+                                budgetExpanded = false
+                                updateSettings()
+                            })
+                        }
+                    }
+                }
+            }
+            Text(text = "Average frame duration: $result")
+        }
     }
 }
